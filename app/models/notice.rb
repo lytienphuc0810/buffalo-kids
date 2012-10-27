92ce2433b7ab3cbf106c1092ec4634b08649d05f
Phuc Ly <lytienphuc0810@gmail.com>
2012-10-27 19:18:57 +0700

    remove fields date... in notice model

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,8 +1,6 @@
 class Notice < ActiveRecord::Base
   attr_accessible 	:title,
   					:content,
-  					:date_created,
-  					:date_updated,
   					:user_id
   validates :title, :content, :date_created, :date_updated, :presence => true
 
--- db/migrate/20121005043804_create_notices.rb
+++ db/migrate/20121005043804_create_notices.rb
@@ -3,8 +3,6 @@ class CreateNotices < ActiveRecord::Migration
 		create_table :notices do |t|
 			t.string :title
 			t.string :content
-			t.date :date_created
-			t.date :date_updated
 			t.integer :user_id
 			t.timestamps
 		end
