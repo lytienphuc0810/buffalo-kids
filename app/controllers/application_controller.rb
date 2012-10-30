4f6b2659e6f31bba26edb6ccbadc3e7d023bcf08
Phuc Ly <lytienphuc0810@gmail.com>
2012-10-31 05:16:14 +0700

    fix devise root link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	def after_sign_in_path_for(resource_or_scope)
+		case resource_or_scope.role
+			when "admin"
+				"/users/index/1"
+			when "user"
+				"/book_reservations/home/1"
+			when "librarian"
+				"/books/index/1"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -71,8 +71,12 @@ BuffaloKids::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'book_reservations#home'
+  devise_for :users do
+    get "/", :to => "devise/sessions#new"
+  end
 
+  root :to => "devise/sessions#new"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
