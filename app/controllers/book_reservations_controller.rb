539eec2f064f267bf3fb41290f796fbb633fc496
Vinh Truong Nguyen <vinh@vinh-Aspire-5750G.(none)>
2012-11-08 00:29:13 +0700

    A bundle of work, hard to tell in one line ;)) --> 2 links: reservation and photocopy registration

--- app/controllers/book_reservations_controller.rb
+++ app/controllers/book_reservations_controller.rb
@@ -26,16 +26,18 @@ class BookReservationsController < ApplicationController
 	end
 
 	def index
-		if current_user.admin?
-			@book_reservations = Book.find_by_id(:book_id).book_reservations.paginate(:page => params[:page])
+		if current_user.admin? || current_user.librarian?
+			@book_reservations = Book.find_by_id(params[:book_id]).book_reservations.paginate(:page => params[:page])
+			if @book_reservations.empty?
+				#error message
+				redirect_to '/books/index/1'
+			end
 		else
 			@book_reservations = current_user.book_reservations.paginate(:page => params[:page])
-		end
-				
-
-		if @book_reservations.empty?
-			#error message
-			redirect_to '/book_reservations/home/1'
+			if @book_reservations.empty?
+				#error message
+				redirect_to '/book_reservations/home/1'
+			end
 		end
 	end
 
@@ -47,6 +49,18 @@ class BookReservationsController < ApplicationController
 	def delete
 		book_reservation = BookReservation.find_by_id(params[:book_reservation_id])
 		book_reservation.destroy
-		redirect_to '/book_reservations/index/1'
+		if current_user.admin? || current_user.librarian?
+			if (book_reservation.book.book_reservations.nil?)
+				redirect_to '/books/index/1'
+			else
+				redirect_to "/book_reservations/index/1?book_id=#{book_reservation.book.id}"
+			end
+		else
+			if (current_user.book_reservations.nil?)
+				redirect_to '/books/index/1'
+			else
+				redirect_to '/book_reservations/index/1'
+			end
+		end
 	end
 end
