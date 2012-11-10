b77938d11e585b0913efabede961138de51a4d63
Phuc Ly <lytienphuc0810@gmail.com>
2012-11-10 20:32:10 +0700

    something for Vu to begin FAQ tab

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ BuffaloKids::Application.routes.draw do
   post "book_instances/update/:book_instance_id" => "book_instances#update"
   post "book_instances/delete/:book_instance_id" => "book_instances#delete"
 
+  get "faq" => "etcs#faq"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
