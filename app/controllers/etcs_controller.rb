ded6540182cb8ffc7bc8f479fbd9b2d28cd43b5a
lytienphuc0810 <lytienphuc0810@gmail.com>
2012-11-14 20:54:47 +0700

    methods to switch between English and Vietnamese

--- app/controllers/etcs_controller.rb
+++ app/controllers/etcs_controller.rb
@@ -6,4 +6,16 @@ class EtcsController < ApplicationController
 	def contact
 		
 	end
+
+	def set_en
+		current_user.locale = "en"
+		current_user.save
+		redirect_to "/"
+	end
+
+	def set_vi
+		current_user.locale = "vi"
+		current_user.save
+		redirect_to "/"
+	end
 end
