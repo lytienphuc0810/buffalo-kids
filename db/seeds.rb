06eddd80c4c1280b503295608ed5ea8d5b45b2a5
Vinh Truong Nguyen <vinh@vinh-Aspire-5750G.(none)>
2012-11-15 12:51:00 +0700

    utf-8??

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 
-# encoding: UTF-8
+# encoding: utf-8
 User.delete_all
 Notice.delete_all
 Book.delete_all
 BookInstance.delete_all
-BookReservation.delete_all
+BookReservation.delete_alls
 PhotoRegistration.delete_all
 
 user1 = User.create(:email => "user1@vit.com", :password => "123456", :username => "user1", :confirmed_at => Time.now)
