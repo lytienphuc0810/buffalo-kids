a686d8ee8509047511190636f38459402dc43470
lytienphuc0810 <lytienphuc0810@gmail.com>
2012-11-26 02:34:54 +0700

    fix scroll problem

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#slider').nivoSlider();
+	if(window.location.pathname == "/" || window.location.pathname == "/users/sign_in" ){
+		$('html, body').animate({
+		    scrollTop: $(".big-content").offset().top
+		}, 500);
+	}
+});
