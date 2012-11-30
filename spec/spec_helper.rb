e8425a65f31fffe32760c1d371a3daf62f51173f
lytienphuc0810 <lytienphuc0810@gmail.com>
2012-12-01 01:01:24 +0700

    comment out solr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,13 +56,12 @@ RSpec.configure do |config|
   config.color_enabled = true
 
   ActionMailer::Base.delivery_method = :test
-
-  config.before(:all) do
-    system 'rake sunspot:solr:start RAILS_ENV=test'
-    sleep(1)
-  end
-  config.after(:all) do
-    system 'rake sunspot:solr:stop RAILS_ENV=test'
-  end 
+  # Run before rspec
+  # config.before(:all) do
+  #   system 'rake sunspot:solr:start RAILS_ENV=test'
+  # end
+  # config.after(:all) do
+  #   system 'rake sunspot:solr:stop RAILS_ENV=test'
+  # end 
 end
 
