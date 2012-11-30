8b3f639ef19b3128fab8766edc549cce8848db47
Vinh T. Nguyen <vinhtruongnguyen@hotmail.com>
2012-11-30 08:35:26 +0700

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(:version => 20121015163700) do
 
   create_table "notices", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
